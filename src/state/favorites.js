63b9861813571bdd0f6f6ee9a34d682691865f98
NazarenoRios <nazarenolrios@gmail.com>
2023-04-18 19:02:13 -0300

    favorite-banner-redux

--- src/state/favorites.js
+++ src/state/favorites.js
@@ -1,10 +1,11 @@
-
 import { createAsyncThunk, createReducer } from "@reduxjs/toolkit";
 import axios from "axios";
 
+import { fetchApi } from "../config/axiosInstance";
+
 // export const addToFavorites = createAsyncThunk("ADD_TO_FAVORITES",(movie, thunkAPI) => {
 //     const { users } = thunkAPI.getState();
-    
+
 //     if (!users.id) throw new Error("You need to be logged in");
 //     return axios
 //       .put(`/api/movies/addFavorite?userId=${users.id}&code=${movie.id}&title=${movie.title}&poster_path=${movie.poster_path}&vote_average=${movie.vote_average}&genres=${movie.genres.map(genre => genre["name"])}`,{code: movie.id, title: movie.title, poster_path: movie.poster_path, vote_average: movie.vote_average, genre: movie.genres.map(genre => genre["name"])})
@@ -12,59 +13,83 @@ import axios from "axios";
 //   }
 // );
 
-export const addToFavorites = createAsyncThunk("ADD_TO_FAVORITES",(movie, thunkAPI) => {
-  const { users } = thunkAPI.getState();
-  
-  if (!users.id) throw new Error("You need to be logged in");
-  return axios
-    .put(`/api/movies/addFavorite?userId=${users.id}&code=${movie.id}&title=${movie.title}&poster_path=${movie.poster_path}&vote_average=${movie.vote_average}&release_date=${movie.release_date}`,{code: movie.id, title: movie.title, poster_path: movie.poster_path, vote_average: movie.vote_average, release_date: movie.release_date})
-    .then((res) => res.data);
-}
+export const addToFavorites = createAsyncThunk(
+  "ADD_TO_FAVORITES",
+  (movie, thunkAPI) => {
+    const { users } = thunkAPI.getState();
+
+    if (!users.id) throw new Error("You need to be logged in");
+    return axios
+      .put(
+        `/api/movies/addFavorite?userId=${users.id}&code=${movie.id}&title=${movie.title}&poster_path=${movie.poster_path}&vote_average=${movie.vote_average}&release_date=${movie.release_date}`,
+        {
+          code: movie.id,
+          title: movie.title,
+          poster_path: movie.poster_path,
+          vote_average: movie.vote_average,
+          release_date: movie.release_date,
+        }
+      )
+      .then((res) => res.data);
+  }
 );
 
-export const removeFromFavorites = createAsyncThunk("REMOVE_FAVORITE",(movie, thunkAPI) => {
-  const { users } = thunkAPI.getState();
-  if (!users.id) throw new Error("You need to be logged in");
-  return axios
-    .delete(`/api/movies/removeFavorite?userId=${users.id}&code=${movie.id}`)
-    .then((res) => res.data);
-}
+export const removeFromFavorites = createAsyncThunk(
+  "REMOVE_FAVORITE",
+  (movie, thunkAPI) => {
+    const { users } = thunkAPI.getState();
+    if (!users.id) throw new Error("You need to be logged in");
+    return axios
+      .delete(`/api/movies/removeFavorite?userId=${users.id}&code=${movie.id}`)
+      .then((res) => res.data);
+  }
 );
 
-export const Favorites = createAsyncThunk("FAVORITES",(setMovies, thunkAPI) => {
-  const { users } = thunkAPI.getState();
-  if (!users.id) throw new Error("You need to be logged in");
-  return axios
-    .get(`/api/movies/favorites?userId=${users.id}`)
-    .then((res) => setMovies(res.data));
-}
+export const Favorites = createAsyncThunk(
+  "FAVORITES",
+  async (setMovies, thunkAPI) => {
+    const { users } = thunkAPI.getState();
+    if (!users.id) return;
+
+    const res = await fetchApi({
+      method: "get",
+      url: `/api/movies/favorites?userId=${users.id}`,
+    });
+
+    await setMovies(res.data);
+    return res.data;
+  }
 );
 
-export const UserFavorites = createAsyncThunk("USER_FAVORITES",({id, setMovies}) => {
-  return axios
-    .get(`/api/movies/favorites?userId=${id}`)
-    .then((res) => setMovies(res.data));
-}
+export const UserFavorites = createAsyncThunk(
+  "USER_FAVORITES",
+  ({ id, setMovies }) => {
+    return axios
+      .get(`/api/movies/favorites?userId=${id}`)
+      .then((res) => setMovies(res.data));
+  }
 );
 
-export const FavoritesRow = createAsyncThunk("FAVORITES_ROW",(setFavorites, thunkAPI) => {
-  const { users } = thunkAPI.getState();
-  if (!users.id) throw new Error("You need to be logged in");
-  return axios
-    .get(`/api/movies/favorites?userId=${users.id}`)
-    .then((res) => setFavorites(res.data));
-}
+export const FavoritesRow = createAsyncThunk(
+  "FAVORITES_ROW",
+  (setFavorites, thunkAPI) => {
+    const { users } = thunkAPI.getState();
+    if (!users.id) throw new Error("You need to be logged in");
+    return axios
+      .get(`/api/movies/favorites?userId=${users.id}`)
+      .then((res) => setFavorites(res.data));
+  }
 );
 
 const favoritesReducer = createReducer(
-    {},
-    {
-        [addToFavorites.fulfilled]: (state,action) => action.payload,
-        [removeFromFavorites.fulfilled]: (state,action) => action.payload,
-        [Favorites.fulfilled]: (state,action) => action.payload,
-        [UserFavorites.fulfilled]: (state,action) => action.payload,
-        [FavoritesRow.fulfilled]: (state,action) => action.payload,
-    }
-)
+  {},
+  {
+    [addToFavorites.fulfilled]: (state, action) => action.payload,
+    [removeFromFavorites.fulfilled]: (state, action) => action.payload,
+    [Favorites.fulfilled]: (state, action) => action.payload,
+    [UserFavorites.fulfilled]: (state, action) => action.payload,
+    [FavoritesRow.fulfilled]: (state, action) => action.payload,
+  }
+);
 
 export default favoritesReducer;
