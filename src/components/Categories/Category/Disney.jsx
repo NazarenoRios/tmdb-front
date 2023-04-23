288e56224b41d94e49480586e761b49a6da997fb
NazarenoRios <nazarenolrios@gmail.com>
2023-04-22 22:22:58 -0300

    disney

--- src/components/Categories/Category/Disney.jsx
+++ src/components/Categories/Category/Disney.jsx
@@ -5,6 +5,8 @@ import Nav from "../../Nav/Nav";
 import CategoryCard from "../../../common/Card/CategoryCard";
 import { CategoryDisneyRequest } from "../../../state/categories";
 import LoadingSpinner from "../../../common/LoadingSpinner";
+import axios from "axios";
+import requests from "../../../utils/requests";
 
 export default function Disney() {
   const get_url = "https://api.themoviedb.org/3";
