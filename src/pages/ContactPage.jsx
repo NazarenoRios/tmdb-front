29abadb79c5bad53b6d1096c859af765325ae097
NazarenoRios <nazarenolrios@gmail.com>
2023-04-23 03:06:08 -0300

    contactPage

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Nav from "../components/Contact/ContactNav";
+import Contact from '../components/Contact/Contact'
 
 const ContactPage = () => {
   return (
     <>
-        <Nav/>
+      <Nav/>
       <Contact/>
     </>
   )
