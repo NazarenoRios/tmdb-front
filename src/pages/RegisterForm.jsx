cdef100df1e21dc4c037e4f74626581a48846770
NazarenoRios <nazarenolrios@gmail.com>
2023-04-23 00:41:10 -0300

    navigates in login and register

--- src/pages/RegisterForm.jsx
+++ src/pages/RegisterForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import RegisterForm from "../components/RegisterForm/RegisterForm";
 import RegisterNav from "../components/RegisterForm/RegisterNav";
